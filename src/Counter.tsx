bd56398ebfbe4534642a4a065fc365af4b920ef3
Marcus Robbins <marcus.robbins@hotmail.co.uk>
2026-04-18 18:11:38 +0100

    Changed the button color to orange in Counter.tsx component.
    Refs #8

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -3,7 +3,10 @@ import { useState } from 'react';
 export function Counter() {
   const [n, setN] = useState(0);
   return (
-    <button onClick={() => setN((x) => x + 1)}>
+    <button 
+      onClick={() => setN((x) => x + 1)}
+      style={{ backgroundColor: 'orange' }}
+    >
       Clicked {n} {n === 1 ? 'time' : 'times'}
     </button>
   );
