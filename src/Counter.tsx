7aa9e43114f7649d9f668593179f2beaca4bb33c
Marcus Robbins <marcus.robbins@hotmail.co.uk>
2026-04-18 17:11:36 +0100

    Updated the counter button to multiply the count by 10 on each click.
    Refs #4

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 export function Counter() {
   const [n, setN] = useState(0);
   return (
-    <button onClick={() => setN((x) => x + 1)}>
+    <button onClick={() => setN((x) => x * 10)}>
       Clicked {n} {n === 1 ? 'time' : 'times'}
     </button>
   );
