455da487d2201579b63c7bf5c0af18b837fe80a6
Marcus Robbins <marcus.robbins@hotmail.co.uk>
2026-04-18 17:29:02 +0100

    Added logic to change the button's background color to red when clicked.
    Refs #5

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -2,8 +2,18 @@ import { useState } from 'react';
 
 export function Counter() {
   const [n, setN] = useState(0);
+  const [isClicked, setIsClicked] = useState(false);
+
+  function handleClick() {
+    setN((x) => x + 1);
+    setIsClicked(true);
+  }
+  
   return (
-    <button onClick={() => setN((x) => x + 1)}>
+    <button 
+      onClick={handleClick} 
+      style={{ backgroundColor: isClicked ? 'red' : 'initial' }}
+    >
       Clicked {n} {n === 1 ? 'time' : 'times'}
     </button>
   );
